b8edfb24aa86e45b28b9af17040681e30c609ca1
Jim Templeton <jimtempleton21@gmail.com>
2024-02-07 17:24:57 -0600

    yes

---
+++ src/App/Copyright.tsx
@@ -0,0 +1,11 @@
+import React, { memo } from 'react'
+
+const Copyright: React.FC = memo(
+  () => (
+    <footer className="info">
+    </footer>
+  ),
+  () => true,
+)
+
+export default Copyright
